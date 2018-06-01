bfaa434e311c5d0a15fc7bbd75802eeef45e622b
siddharth <sidforyou14@gmail.com>
2018-06-01 14:46:33 +0530

    admin panel added

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+            if(auth::check())
+            {
+                echo 'authenticated';
+               // return redirect('home')->with('error','You have not admin access');
+            }
+            echo 'not authenticated';
+        return redirect('home')->with('error','You have not admin access');
+
+
+    }
+
+}
